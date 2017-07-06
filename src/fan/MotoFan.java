359c764dbf967a6c3c45f1148b6d976d3f5c1cf6
Hemantha <Hemantha@LAPTOP-IINUTUEU>
2017-07-07 00:48:48 +0530

    qwerty143

---
+++ src/fan/MotoFan.java
@@ -0,0 +1,15 @@
+package fan;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MotoFan {
+	@Test
+	public void TestA(){
+		Reporter.log("hello",true);
+		
+	}
+
+}
+
+
